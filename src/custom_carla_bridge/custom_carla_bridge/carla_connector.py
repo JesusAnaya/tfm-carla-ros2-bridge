b31db30556f85a076854e60f342d556aa408bb08
Jesus Armando Anaya <jesus@armandoanaya.com>
2023-05-13 19:09:04 -0700

    Split Carla connector and ROS node in 2 different files.
    Added 3 cameras to the car (center, left, right), and added the respective publishers.

---
+++ src/custom_carla_bridge/custom_carla_bridge/carla_connector.py
@@ -0,0 +1,140 @@
+import carla
+import random
+from typing import Any, Callable
+
+
+class CarlaConnector(object):
+    def __init__(self, host: str, town: str) -> None:
+        self.host = host
+        self.town = town
+        self.world: [carla.World, None] = None
+        self.ego_vehicle: [carla.Vehicle, None] = None
+        self.camera_center: [carla.Sensor, None] = None
+        self.camera_left: [carla.Sensor, None] = None
+        self.camera_right: [carla.Sensor, None] = None
+        self.client: [carla.Client, None] = None
+        self.camera_deviation: float = 0.4
+
+    def connect_to_carla(self) -> None:
+        # Connect to the Carla simulator
+        self.client = carla.Client(self.host, 2000)
+        self.client.set_timeout(200.0)
+
+        # Load the town
+        self.init_world()
+
+        # Get the blueprint library
+        blueprint_library = self.world.get_blueprint_library()
+
+        # Set the weather
+        self.init_weather()
+
+        # init ego vehicle
+        self.init_ego_vehicle(blueprint_library)
+
+        self.init_cameras(blueprint_library)
+
+    def init_world(self) -> None:
+        # Load the town
+        self.world = self.client.load_world(self.town)
+        settings = self.world.get_settings()
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.05
+        self.world.apply_settings(settings)
+        self.world.tick()
+
+    def init_weather(self) -> None:
+        # Get the current weather parameters
+        weather = self.world.get_weather()
+
+        # Set the desired parameters
+        weather.cloudiness = 0.0
+        weather.precipitation = 0.0
+        weather.fog_density = 0.0
+        weather.sun_altitude_angle = 10.0
+
+        # Apply the new weather parameters
+        self.world.set_weather(weather)
+
+    def init_ego_vehicle(self, blueprint_library: Any) -> None:
+        # Load the Tesla model
+        tesla_blueprint = blueprint_library.find("vehicle.tesla.model3")
+
+        # Spawn the Tesla at a random spawn point
+        spawn_point = random.choice(self.world.get_map().get_spawn_points())
+        self.ego_vehicle = self.world.try_spawn_actor(tesla_blueprint, spawn_point)
+        self.world.tick()
+
+    def init_cameras(self, blueprint_library: Any) -> None:
+        # Attach an RGB camera to the Tesla
+        camera_bp = blueprint_library.find("sensor.camera.rgb")
+        camera_bp.set_attribute("image_size_x", "800")
+        camera_bp.set_attribute("image_size_y", "600")
+        camera_bp.set_attribute("fov", "90")
+
+        # Get Vehicle Physics Control
+        physics_control = self.ego_vehicle.get_physics_control()
+        # Get the maximum steer angle of the front wheels
+        max_steer_angle = physics_control.wheels[0].max_steer_angle
+
+        # Calculate the steering angle for the cameras
+        left_camera_angle = -max_steer_angle * self.camera_deviation
+        right_camera_angle = max_steer_angle * self.camera_deviation
+
+        # Set cameras to the center, left, and right of the vehicle
+        self.camera_center = self.set_camera(
+            camera_bp, carla.Transform(carla.Location(x=2.5, z=1.2))
+        )
+        self.camera_right = self.set_camera(
+            camera_bp,
+            carla.Transform(
+                carla.Location(x=2.5, z=1.2), carla.Rotation(yaw=right_camera_angle)
+            ),
+        )
+        self.camera_left = self.set_camera(
+            camera_bp,
+            carla.Transform(
+                carla.Location(x=2.5, z=1.2), carla.Rotation(yaw=left_camera_angle)
+            ),
+        )
+
+    def set_camera(
+        self, camera_bp: carla.Sensor, transform: carla.Transform
+    ) -> carla.Sensor:
+        return self.world.spawn_actor(camera_bp, transform, attach_to=self.ego_vehicle)
+
+    def set_camera_center_callback(self, callback: Callable) -> None:
+        # Register the image callback
+        self.camera_center.listen(callback)
+
+    def set_camera_left_callback(self, callback: Callable) -> None:
+        # Register the image callback
+        self.camera_left.listen(callback)
+
+    def set_camera_right_callback(self, callback: Callable) -> None:
+        # Register the image callback
+        self.camera_right.listen(callback)
+
+    def timer_callback(self) -> None:
+        self.world.tick()
+
+    def set_vehicle_control(self, control: carla.VehicleControl) -> None:
+        self.ego_vehicle.apply_control(control)
+
+    def cleanup(self) -> None:
+        # Cleanup the Carla ROS node
+        if self.camera_center is not None:
+            self.camera_center.destroy()
+
+        if self.camera_left is not None:
+            self.camera_left.destroy()
+
+        if self.camera_right is not None:
+            self.camera_right.destroy()
+
+        if self.ego_vehicle is not None:
+            self.ego_vehicle.destroy()
+
+        settings = self.world.get_settings()
+        settings.synchronous_mode = False
+        self.world.apply_settings(settings)
--- src/custom_carla_bridge/custom_carla_bridge/carla_ros_node.py
+++ src/custom_carla_bridge/custom_carla_bridge/carla_ros_node.py
@@ -1,141 +1,119 @@
-import rclpy
-import cv2
-import carla
-import numpy as np
-import random
-import signal
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
 from rclpy.executors import SingleThreadedExecutor
 from sensor_msgs.msg import Image
-from std_msgs.msg import Float32
 from custom_interfaces.msg import VehicleControl
+from .carla_connector import CarlaConnector
 from cv_bridge import CvBridge
+from typing import Any
+import rclpy
+import carla
+import signal
+import numpy as np
+
+
+# Create a QoS profile with "best effort" reliability
+qos_profile = QoSProfile(
+    depth=24,  # Adjust this as necessary
+    reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+    durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE,
+)
+cv_bridge = CvBridge()
+
+
+def process_image(carla_image: carla.Image, publisher: Any) -> None:
+    # Convert the CARLA image from BGRA to RGB format
+    image_bgra = np.array(carla_image.raw_data).reshape(
+        (carla_image.height, carla_image.width, 4)
+    )
+    image_rgb = image_bgra[:, :, :3][:, :, ::-1]
+
+    # Convert the OpenCV image to a ROS Image message
+    image_msg = cv_bridge.cv2_to_imgmsg(image_rgb, encoding="rgb8")
+
+    publisher.publish(image_msg)
 
 
 class CarlaROSNode(Node):
     def __init__(self):
-        super().__init__('carla_module_node')
-        self.world = None
-        self.ego_vehicle = None
-        self.camera = None
-        self.client = None
-
-        self.cv_bridge = CvBridge()
-
+        super().__init__("carla_module_node")
         # Parameters
-        self.declare_parameter('host', 'localhost')
-        self.host = self.get_parameter('host').value
+        self.camera_right_publisher: Any = None
+        self.camera_left_publisher: Any = None
+        self.camera_center_publisher: Any = None
+        self.vehicle_control_sub: Any = None
 
-        self.declare_parameter('town', 'Town01_Opt')
-        self.town = self.get_parameter('town').value
+        self.declare_parameter("host", "localhost")
+        self.declare_parameter("town", "Town01_Opt")
+        host: str = self.get_parameter("host").value
+        town: str = self.get_parameter("town").value
 
-        # Create a QoS profile with "best effort" reliability
-        qos_profile = QoSProfile(
-            depth=24,  # Adjust this as necessary
-            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-            durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE,
-        )
+        self.carla_connector = CarlaConnector(host, town)
 
         # Publishers
-        self.image_pub = self.create_publisher(Image, 'carla/vehicle/camera', qos_profile)
+        self.set_publishers()
 
         # Subscribers
-        self.vehicle_control_sub = self.create_subscription(
-            VehicleControl,
-            '/carla/vehicle/control',
-            self.vehicle_control_callback,
-            10
-        )
+        self.set_subscribers()
 
         # Shutdown callback
         signal.signal(signal.SIGINT, self.shutdown_callback)
 
         # Connect to the Carla simulator
-        self.connect_to_carla()
+        self.get_logger().info("Connecting to Carla...")
+        self.carla_connector.connect_to_carla()
 
-        self.timer_period = 0.05  # desired simulation rate in seconds (e.g., 0.05 s for 20 Hz)
-        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        # Set camera callbacks
+        self.get_logger().info("Setting camera callbacks...")
+        self.carla_connector.set_camera_center_callback(self.camera_center_callback)
+        self.carla_connector.set_camera_left_callback(self.camera_left_callback)
+        self.carla_connector.set_camera_right_callback(self.camera_right_callback)
 
-    def connect_to_carla(self) -> None:
-        # Connect to the Carla simulator
-        self.client = carla.Client(self.host, 2000)
-        self.client.set_timeout(10.0)
+        # desired simulation rate in seconds (e.g., 0.05 s for 20 Hz)
+        self.timer_period: float = 0.05
+        self.timer: Any = self.create_timer(
+            self.timer_period, self.carla_connector.timer_callback
+        )
 
-        # Load the town
-        self.world = self.client.load_world(self.town)
-        settings = self.world.get_settings()
-        settings.synchronous_mode = True
-        settings.fixed_delta_seconds = 0.05
-        self.world.apply_settings(settings)
-        self.world.tick()
+    def set_publishers(self):
+        self.camera_center_publisher = self.create_publisher(
+            Image, "carla/vehicle/camera_center", qos_profile
+        )
 
-        # Get the current weather parameters
-        weather = self.world.get_weather()
+        self.camera_left_publisher = self.create_publisher(
+            Image, "carla/vehicle/camera_left", qos_profile
+        )
 
-        # Set the desired parameters
-        weather.cloudiness = 0.0
-        weather.precipitation = 0.0
-        weather.fog_density = 0.0
-        weather.sun_altitude_angle = 10.0
+        self.camera_right_publisher = self.create_publisher(
+            Image, "carla/vehicle/camera_right", qos_profile
+        )
 
-        # Apply the new weather parameters
-        self.world.set_weather(weather)
+    def set_subscribers(self):
+        self.vehicle_control_sub = self.create_subscription(
+            VehicleControl, "/carla/vehicle/control", self.vehicle_control_callback, 10
+        )
 
-        # Load the Tesla model
-        blueprint_library = self.world.get_blueprint_library()
-        tesla_blueprint = blueprint_library.find('vehicle.tesla.model3')
+    def camera_center_callback(self, image: carla.Image) -> None:
+        process_image(image, self.camera_center_publisher)
 
-        # Spawn the Tesla at a random spawn point
-        spawn_point = random.choice(self.world.get_map().get_spawn_points())
-        self.ego_vehicle = self.world.try_spawn_actor(tesla_blueprint, spawn_point)
-        self.world.tick()
+    def camera_left_callback(self, image: carla.Image) -> None:
+        process_image(image, self.camera_left_publisher)
 
-        # Attach an RGB camera to the Tesla
-        camera_bp = blueprint_library.find('sensor.camera.rgb')
-        camera_bp.set_attribute('image_size_x', '800')
-        camera_bp.set_attribute('image_size_y', '600')
-        camera_bp.set_attribute('fov', '110')
-
-        camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-        self.camera = self.world.spawn_actor(camera_bp, camera_transform, attach_to=self.ego_vehicle)
-
-        # Register the image callback
-        self.camera.listen(lambda image: self.process_image(image))
-
-    def timer_callback(self):
-        self.world.tick()
-
-    def process_image(self, carla_image: carla.Image) -> None:
-        # Convert the CARLA image from BGRA to RGB format
-        bgra_image = np.array(carla_image.raw_data).reshape((carla_image.height, carla_image.width, 4))
-        rgb_image = bgra_image[:, :, :3][:, :, ::-1]
-
-        # Convert the OpenCV image to a ROS Image message
-        image_msg = self.cv_bridge.cv2_to_imgmsg(rgb_image, encoding='rgb8')
-
-        self.image_pub.publish(image_msg)
+    def camera_right_callback(self, image: carla.Image) -> None:
+        process_image(image, self.camera_right_publisher)
 
     def vehicle_control_callback(self, msg: VehicleControl) -> None:
         # Apply the control commands to the ego vehicle
         # self.get_logger().info(f'Applying control: {msg}')
 
         control = carla.VehicleControl(
-            throttle=msg.throttle,
-            steer=msg.steer,
-            brake=msg.brake,
-            reverse=msg.reverse
+            throttle=msg.throttle, steer=msg.steer, brake=msg.brake, reverse=msg.reverse
         )
-        self.ego_vehicle.apply_control(control)
+        self.carla_connector.set_vehicle_control(control)
 
     def shutdown_callback(self) -> None:
-        # Cleanup the Carla ROS node
-        self.camera.destroy()
-        self.ego_vehicle.destroy()
-
-        settings = self.world.get_settings()
-        settings.synchronous_mode = False
-        self.world.apply_settings(settings)
+        self.get_logger().info("Cleaning up Carla connector...")
+        self.carla_connector.cleanup()
 
 
 def main(args=None) -> None:
@@ -154,5 +132,5 @@ def main(args=None) -> None:
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
